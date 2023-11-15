830b2dbddcc6536f6efb4ea7b353d090b159e8b3
Joel Ye <joelye9@gmail.com>
2023-11-15 12:41:56 -0500

    unapply

--- context_general_bci/tasks/preproc_utils.py
+++ context_general_bci/tasks/preproc_utils.py
@@ -35,11 +35,26 @@ def apply_minmax_norm(covariates: torch.Tensor | np.ndarray, norm: Dict[str, tor
     """
     covariates = torch.as_tensor(covariates, dtype=torch.float)
     if norm['cov_mean'] is not None and norm['cov_min'] is not None and norm['cov_max'] is not None:
+        # TODO bug here - we don't have the rescale clamping we previously had.
         covariates = (covariates - norm['cov_mean'][:covariates.size(-1)]) / (norm['cov_max'][:covariates.size(-1)] - norm['cov_min'][:covariates.size(-1)])
     else:
         covariates = covariates / norm['cov_max'][:covariates.size(-1)]
     covariates = torch.clamp(covariates, -1, 1)
     return covariates, norm
+
+def unapply_minmax_norm(covariates: torch.Tensor | np.ndarray, norm: Dict[str, torch.Tensor | None]) -> Tuple[torch.Tensor, Dict[str, torch.Tensor | None]]:
+    r"""
+        Apply min/max normalization for covariates
+        covariates: ... H  trailing dim is covariate dim
+        noise_suppression: H - clip away values under this magnitude
+    """
+    covariates = torch.as_tensor(covariates, dtype=torch.float)
+    if norm['cov_mean'] is not None and norm['cov_min'] is not None and norm['cov_max'] is not None:
+        # TODO bug here - we don't have the rescale clamping we previously had.
+        covariates = covariates * (norm['cov_max'][:covariates.size(-1)] - norm['cov_min'][:covariates.size(-1)]) + norm['cov_mean'][:covariates.size(-1)]
+    else:
+        covariates = covariates * norm['cov_max'][:covariates.size(-1)]
+    return covariates
 
 def get_minmax_norm(covariates: torch.Tensor | np.ndarray, center_mean=False) -> Tuple[torch.Tensor, Dict[str, torch.Tensor | None]]:
     r"""
